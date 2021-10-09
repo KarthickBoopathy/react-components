c853bebaa5fbf9b726c0a8bebcb9b59d4e00b1b4
KarthickBoopathy <Karthickboopathy94@gmail.com>
2021-10-09 21:45:39 +0530

    initial-commit

---
+++ src/common/style.ts
@@ -0,0 +1,12 @@
+export const style = {
+  productSearchListImage: {
+    maxWidth: "-webkit-fill-available",
+    cursor: "pointer",
+  },
+  textPointer: {
+    cursor: "pointer",
+    "&:hover": {
+      color: "red",
+    },
+  },
+};
