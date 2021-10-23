238e69e75c89e956ed8450d8801117208b5a4d4a
KarthickBoopathy <Karthickboopathy94@gmail.com>
2021-10-23 22:47:50 +0530

    rebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+<<<<<<< HEAD
 import NavBars from './components/NavBars/NavBars';
 
 
@@ -8,6 +9,17 @@ function App() {
     <React.Fragment>
       <NavBars />
     </React.Fragment>
+=======
+import ProductSearchList from './components/ProductSearchList';
+import GridItems from './components/GridItems';
+
+function App() {
+  return (
+    <>
+       {/* <ProductSearchList /> */}
+      <GridItems />
+    </>
+>>>>>>> aca9c6d... grid-items
   );
 }
 
