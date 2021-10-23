b40c7cf3e9f92accfaffaaaec9354018a80af0bf
KarthickBoopathy <Karthickboopathy94@gmail.com>
2021-10-23 22:47:07 +0530

    rebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import NavBars from './components/NavBars/NavBars';
 
+
 function App() {
   return (
     <React.Fragment>
--- src/components/GridItems.tsx
+++ src/components/GridItems.tsx
@@ -1,59 +1,68 @@
-import { Box, Card, CardActionArea, CardContent, CardMedia, Grid, Paper, styled, Typography } from "@mui/material";
+import { Grid, Link, Paper } from "@mui/material";
+import { Box } from "@mui/system";
+import { useEffect, useState } from "react";
 
 const itemData = [
-    {
-      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-      title: 'Mens Collection | Under 499',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-      title: 'Womens Collection | Under 499',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-      title: 'Mens Footwear | Under 499',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-      title: 'Womens Featwear | Under 499',
-    },
-  ];
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Mens Collection | Under 499",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Womens Collection | Under 499",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Mens Footwear | Under 499",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Womens Featwear | Under 499",
+  },
+];
 
-const GridItems= () => {
+const GridItems = () => {
+  const [data, SetData] = useState<any[]>([]);
 
-    
-    return(
-        <Box>
+  useEffect(() => {
+    SetData(itemData);
+  }, []);
 
-  <Typography variant="h5" gutterBottom component="div">
-        Under ₹599 | Pocket-friendly Fashion
-        </Typography>
-
-         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          {itemData.map((item) => (
-            <Grid item xs={6}>
-              <Card sx={{ maxWidth: 345 }}>
-  
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    image={item.img}
-                    alt="green iguana"
-                  />
-                  <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                      {item.title}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
+  return (
+    <>
+      <Paper square elevation={3}>
+        <Grid container>
+          {data.map((item, i) => (
+            <Grid
+              item
+              key={i}
+              xs={6}
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Box
+                component="img"
+                sx={{
+                  p: 2,
+                  maxWidth: "-webkit-fill-available",
+                }}
+                src={item.img}
+                alt="image"
+              />
             </Grid>
-  
           ))}
-        </Grid>
-      </Box>
-    );
 
+          <Grid item xs={12}>
+            <Link sx={{ p: 2, fontSize: "small" }}>Show more</Link>
+          </Grid>
+        </Grid>
+        <br />
+      </Paper>
+    </>
+  );
 };
+
 export default GridItems;
